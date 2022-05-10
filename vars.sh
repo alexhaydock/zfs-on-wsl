82654a365f85ab81297a9c25cbd9201cced9a011
Alex Haydock <alex@alexhaydock.co.uk>
2022-05-10 18:42:49 +0100

    Try 5.15 to see if it fixes build failure with 5.17

--- vars.sh
+++ vars.sh
@@ -3,6 +3,6 @@
 # Define the Linux Kernel OpenZFS version we want to build in
 # this file, as well as our custom kernel name.
 
-export KERNELVER="5.17.6"  # https://www.kernel.org/
+export KERNELVER="5.15.38"  # https://www.kernel.org/
 export ZFSVER="2.1.4"  # https://zfsonlinux.org/
 export KERNELNAME="penguins-rule"
